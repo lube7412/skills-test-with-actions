d1cb3086c09f801a7320d5aaf8c3f9404aec5ec7
lube7412 <lucy.beck1@tha.de>
2026-05-04 14:03:55 +0000

    Fix coverage tests

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -69,12 +69,11 @@ def test_get_nth_fibonacci_ten():
     # Assert
     assert result == 55
 
-    def test_area_of_circle_negative_radius():
+def test_area_of_circle_negative_radius():
     """Test with a negative radius."""
     with pytest.raises(ValueError):
         area_of_circle(-1)
-
-
+    
 def test_get_nth_fibonacci_negative():
     """Test with negative n."""
     with pytest.raises(ValueError):
